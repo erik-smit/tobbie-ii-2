fb5aed062fdabf6323e54b6482f9b7f4cf4ce606
Erik Smit <erik.lucas.smit@gmail.com>
2021-11-13 14:41:43 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -27,17 +27,30 @@ function tobbieTurn (speed: number) {
         ))
     }
 }
+// Protocol:
+// Function,Value,Value
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    speeds = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)).split(",")
-    walkSpeed = parseFloat(speeds[0])
-    turnSpeed = parseFloat(speeds[1])
-    tobbieTurn(turnSpeed)
-    tobbieWalk(walkSpeed)
+    rxData = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    serial.writeLine(rxData)
+    rxString = _py.py_string_split(rxData, ",")
+rxFunction = rxString[0]
+    if (rxFunction == "V") {
+    	
+    } else if (rxFunction == "S") {
+        walkSpeed = parseFloat(rxString[1])
+        turnSpeed = parseFloat(rxString[2])
+        tobbieTurn(turnSpeed)
+        tobbieWalk(walkSpeed)
+    } else if (rxFunction == "T") {
+        displayText = rxString[1]
+    } else {
+        basic.showIcon(IconNames.Heart)
+    }
 })
-function tobbieWalk (speed: number) {
-    if (speed >= 0) {
+function tobbieWalk (speed2: number) {
+    if (speed2 >= 0) {
         pins.analogWritePin(AnalogPin.P13, pins.map(
-        speed,
+        speed2,
         0,
         100,
         0,
@@ -47,7 +60,7 @@ function tobbieWalk (speed: number) {
     } else {
         pins.analogWritePin(AnalogPin.P13, 0)
         pins.analogWritePin(AnalogPin.P14, pins.map(
-        speed,
+        speed2,
         0,
         -100,
         0,
@@ -55,16 +68,30 @@ function tobbieWalk (speed: number) {
         ))
     }
 }
+let prevDisplayText = ""
+let displayText = ""
 let turnSpeed = 0
 let walkSpeed = 0
-let speeds: string[] = []
+let rxFunction = ""
+let speeds: number[] = []
+let rxData = ""
+let rxString: string[] = []
 basic.showIcon(IconNames.SmallDiamond)
 bluetooth.startButtonService()
 bluetooth.startIOPinService()
 bluetooth.startAccelerometerService()
 bluetooth.startLEDService()
 bluetooth.startUartService()
+pins.analogSetPeriod(AnalogPin.P13, 1000)
+pins.analogSetPeriod(AnalogPin.P14, 1000)
+pins.analogSetPeriod(AnalogPin.P15, 1000)
+pins.analogSetPeriod(AnalogPin.P16, 1000)
 basic.showIcon(IconNames.Diamond)
 basic.forever(function () {
-	
+    if (prevDisplayText != displayText) {
+        prevDisplayText = displayText
+        basic.clearScreen()
+        basic.showString(displayText)
+        basic.pause(500)
+    }
 })
